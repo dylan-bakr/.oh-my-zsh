796c4027044a3f97462a2bcc1b175ef2a1d806a5
Dylan Bakr <Dylan.Bakr@milliman.com>
2025-01-14 09:36:40 -0500

    update aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,4 +1,7 @@
-alias adnew="grep -n \"ZSH_THEME_RANDOM_CANDIDATES\" .zshrc | cut -c1-2 | xargs -I {} sh -c \"sed -i.bak '{}s/\" )/\" \"$RANDOM_THEME\" )/' ~/.zshrc\""
+alias addnew="grep -n \"ZSH_THEME_RANDOM_CANDIDATES\" .zshrc | cut -c1-2 | xargs -I {} sh -c \"sed -i.bak '{}s/\" )/\" \"$RANDOM_THEME\" )/' ~/.zshrc\""
 alias cls='clear && ls'
-alias edtals='nano ~/.oh-my-zsh/custom/aliases.zsh'
+alias aliases='nano ~/.oh-my-zsh/custom/aliases.zsh'
 alias prp='poetry run pytest'
+alias yoink='git pull'
+alias yeet='git push'
+alias hmmm='git fetch'
